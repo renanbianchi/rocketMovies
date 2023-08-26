4cb73c8c96bd14dc6a25b4e287b2c539b5c76704
renan <renanbianchi@gmail.com>
2023-08-26 17:19:33 -0300

    fix: Removed ToLowerCase from Name field to fix Name always displaying in lowercase

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -57,7 +57,7 @@ export function SignUp() {
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assistir.</p>
         <h2>Crie sua conta</h2>
-        <Input placeholder="Nome Completo" type="text" icon={FiUser} onKeyPress={handleKeyPress} onChange={e => setUserInfos(prev => ({...prev, name: e.target.value.trim().toLowerCase()}))}/>
+        <Input placeholder="Nome Completo" type="text" icon={FiUser} onKeyPress={handleKeyPress} onChange={e => setUserInfos(prev => ({...prev, name: e.target.value.trim()}))}/>
         <Input placeholder="E-mail" type="text" icon={FiMail} onKeyPress={handleKeyPress} onChange={e => setUserInfos(prev => ({...prev, email: e.target.value.trim().toLowerCase()}))} />
         <Input placeholder="Senha" type="password" icon={FiLock} onKeyPress={handleKeyPress} onChange={e => setUserInfos(prev => ({...prev, password1: e.target.value}))} />
         <Input placeholder="Confirme sua senha" type="password" icon={FiLock} onKeyPress={handleKeyPress} margin="4px 0 16px 0" onChange={e => setUserInfos(prev => ({...prev, password2: e.target.value}))} />
