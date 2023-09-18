abc927852e58794eb8937676cc027b6681a9bb54
renan <renanbianchi@gmail.com>
2023-09-17 21:35:12 -0300

    Chore: better implementation of Button Component

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
-  width: 100%;
+  width: ${({fit}) => fit ? 'fit-content' : '100%'};
   background-color: ${({ theme }) => theme.COLORS.PINK };
   color: ${({ theme }) => theme.COLORS.BACKGROUND_800 };
   height: ${({height}) => height ? height : "auto"};
